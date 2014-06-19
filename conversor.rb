42d21c5c04eeb47d93537d405eea778620c0f829
namelivia <ohcan2@gmail.com>
2014-06-19 18:51:37 +0200

    Commit inicial

---
+++ conversor.rb
@@ -0,0 +1,86 @@
+#Conversor de mis cuentas, esta vez en Ruby
+require 'nokogiri'
+require 'csv'
+
+def pregunta(texto)
+	while true
+		print texto+" [s/n]: "
+		case $stdin.gets.strip
+		when 's', 'S'
+			return true
+		when /\A[nN]o?\Z/
+			return false
+		end
+	end
+end
+
+def selectorCategorias(categorias,nombre)
+	while true
+		print "Seleccione la categoría:\n"
+		categorias.each_with_index do |categoria,index|
+			print index.to_s+":"+categoria[0]+"\n"
+		end
+		selected = $stdin.gets.strip.to_i
+		if selected.between?(0,categorias.length-1)
+			categorias[selected] << nombre
+			@categoriasEditado = true
+			return categorias[selected][0]
+		end
+	end
+end
+
+categorias = CSV.read("categorias")
+@categoriasEditado = false
+if ARGV.length != 1
+	print "ERROR: Parámetro de entrada no introducido\nUso: ruby conversor [archivo de entrada]\n"
+	exit 
+end
+input = File.open(ARGV.first);
+output = File.open("resultado.csv","w")
+tabla = Nokogiri::XML(input).css('table.datos')
+
+tabla.children.each do |fila|
+	nombre = fila.children[0].text
+	info = fila.children[3].text 
+
+	fecha = fila.children[1].text
+	fecha["/"] = "-"
+	fecha["/20"] = "-"
+
+	importe = fila.children[4].text
+	importe = importe.gsub('.','').gsub(',','.')
+
+	#Asigna la categoría en funcion del nombre
+	categoria = '' 
+	categorias.each do |tipoCategoria|
+		if tipoCategoria.include? nombre
+			categoria = tipoCategoria[0]
+		end
+	end
+
+	if categoria == ''
+		#Si no ha encontrado categoría, preguntará si añadirla a una existente.
+		print("Nombre: "+nombre+"\n");
+		print("Fecha: "+fecha+"\n");
+		print("Importe: "+importe+"\n");
+		if pregunta("No se ha encontrado categoría para la transacción. ¿Desea añadirla a una existente?")
+			#Selecciona la categoría
+			categoria = selectorCategorias(categorias,nombre)
+		else
+			print("Introduzca un texto descriptivo de la transacción:\n")
+			info = $stdin.gets.strip
+		end
+	end
+	output.puts(fecha+";0;"+nombre+";;"+info+";"+importe+";"+categoria+";");
+end
+output.close
+input.close
+if (@categoriasEditado)
+	CSV.open("categorias", "w") do |categoriasNuevo|
+		categorias.each do |tipoCategoria|
+			categoriasNuevo << tipoCategoria 
+		end
+	end
+	print("El archivo categorias se ha actualizado\n")
+end
+print("Resultados escritos en el archivo resultado.csv\n")
