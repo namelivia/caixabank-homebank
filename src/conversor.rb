0e6dc5d2b95fb2392af3926558b57b08f463ad88
Nacho Amelivia <ohcan2@gmail.com>
2018-10-26 17:41:24 +0200

    Separating the code with ugly comments to start refactoring

--- src/conversor.rb
+++ src/conversor.rb
@@ -8,6 +8,7 @@ require 'qif'
 class Conversor
 
 	def convertir()
+		###############LOCALE##########
 		#Sets the available locales
 		I18n.config.available_locales = 'es', 'en'
 		#Autodetects the locale or sets the default
@@ -16,22 +17,35 @@ class Conversor
 		rescue 
 			I18n.locale = I18n.default_locale
 		end
-		I18n.load_path << Dir[File.expand_path(File.join(File.dirname(__FILE__), "../locales")) + "/*.yml"]
-		categorias = CSV.read(File.join(File.dirname(__FILE__), "../categorias"))
+		#Loads the locale
+		I18n.load_path << Dir[File.expand_path(File.join(File.dirname(__FILE__), '../locales')) + '/*.yml']
+		#################################
+		##############INITIALIZES CATEGORIES####################################
+		categorias = CSV.read(File.join(File.dirname(__FILE__), '../categorias'))
 		@categoriasEditado = false
+		#######################################################################
+		##############CHECKS INPUT#############################################
 		if ARGV.length != 1 || ARGV[0].split(//).last(4).join != '.xls'
 			print I18n.t(:invalid_params)
 			exit 
 		end
+		#######################################################################
+		###########################OPENS THE INPUT FILE########################
 		input = Spreadsheet.open(ARGV.first).worksheet 0;
-		Qif::Writer.open(File.join(File.dirname(__FILE__), "../resultado.qif"), "Bank") do |writer|
-			3.upto(input.row_count-1) do |row|
-				nombre = input[row,0].to_s
-				info = input[row,3].to_s
-				fecha = input[row,1].strftime("%d-%m-%Y")
-				importe = input[row,4].to_s
+		###################################################
+		###########################OPENS THE OUTPUT FILE########################
+		Qif::Writer.open(File.join(File.dirname(__FILE__), '../resultado.qif'), 'Bank') do |writer|
+			#File operation
+			#skips the first 3 rows
+			3.upto(input.row_count - 1) do |row|
+				#Gets fields
+				nombre = input[row, 0].to_s
+				info = input[row, 3].to_s
+				fecha = input[row, 1].strftime("%d-%m-%Y")
+				importe = input[row, 4].to_s
 
-				#Asigna la categoría en funcion del nombre
+				################## CATEGORY SELECTION #############################
+				#automatic category selection by name
 				categoria = '' 
 				categorias.each do |tipoCategoria|
 					if tipoCategoria.include? nombre
@@ -39,19 +53,22 @@ class Conversor
 					end
 				end
 
+				#Si no ha encontrado categoría, preguntará si añadirla a una existente.
 				if categoria == ''
-					#Si no ha encontrado categoría, preguntará si añadirla a una existente.
-					print(I18n.t(:name)+nombre+"\n");
-					print(I18n.t(:date)+fecha+"\n");
-					print(I18n.t(:amount)+importe+"\n");
+					print(I18n.t(:name) + nombre + "\n");
+					print(I18n.t(:date) + fecha + "\n");
+					print(I18n.t(:amount) + importe + "\n");
 					if pregunta(I18n.t(:select_category))
-						#Selecciona la categoría
+						#Selecciona la categoría usando el selector de categorías
 						categoria = selectorCategorias(categorias,nombre)
 					else
 						print(I18n.t(:input_info))
 						info = $stdin.gets.strip
 					end
 				end
+				###############################################
+				
+				#Writes on the file
 				writer << Qif::Transaction.new(
 					:date => fecha,
 					:amount => importe,
@@ -60,6 +77,9 @@ class Conversor
 				)
 			end
 		end
+
+		#############################UPDATES THE CATEGORY FILE##################################
+		#Does this always generate new values?
 		if (@categoriasEditado)
 			CSV.open(File.join(File.dirname(__FILE__), "../categorias"), "w") do |categoriasNuevo|
 				categorias.each do |tipoCategoria|
@@ -70,7 +90,9 @@ class Conversor
 		end
 		print(I18n.t(:file_generated))
 	end
+	###############################################
 
+	######################CONFIRM DIALOG#########################
 	def pregunta(texto)
 		while true
 			print texto+I18n.t(:confirm_or_deny)
@@ -82,7 +104,9 @@ class Conversor
 			end
 		end
 	end
+	#############################################################
 
+	#####################CATEGORY SELECTOR########################
 	def selectorCategorias(categorias,nombre)
 		while true
 			print I18n.t(:choose_category)
@@ -97,4 +121,5 @@ class Conversor
 			end
 		end
 	end
+	##############################################################
 end
